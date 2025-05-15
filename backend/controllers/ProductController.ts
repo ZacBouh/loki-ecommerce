b01f1efc11a4a7134261fa192fbc6701c721257c
ZacBouh <zacharie.bouhaya@gmail.com>
2025-05-15 14:30:39 +0000

    fixed data seeder, updated tsconfig and working ProductController.getProducts

--- backend/controllers/ProductController.ts
+++ backend/controllers/ProductController.ts
@@ -1,9 +1,10 @@
-import { NextFunction, RequestHandler } from "express";
+import { RequestHandler } from "express";
 import { Product } from "../models/product.js";
 export default class ProductController {
 
     static getProducts : RequestHandler = async (req , res , next) => {
-        const products = Product.find()
+        const products = await Product.find().exec()
+        console.log("found products ", products)
         res.json(products)
     }
 

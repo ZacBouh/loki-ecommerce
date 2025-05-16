9dcf3704b5feeb765d960ebf8dbd58592f5e6e20
ZacBouh <zacharie.bouhaya@gmail.com>
2025-05-16 09:04:49 +0000

    backend serving front end app, app:start script

--- backend/controllers/ProductController.ts
+++ backend/controllers/ProductController.ts
@@ -3,10 +3,23 @@ import { Product } from "../models/product.js";
 
 export default class ProductController {
 
+    static logger(...args : any){
+        
+    }
+
     static getProducts : RequestHandler = async (req , res , next) => {
         const products = await Product.find().exec()
-        console.log("found products ", products)
         res.json(products)
+    }
+
+    static getProduct : RequestHandler = async (req, res, next) => {
+        const productId = req.params.id
+        const product = await Product.findById(productId).exec()
+        if(!product){
+            res.status(201)
+            return
+        }
+        console.log("found product ", product.id)
     }
 
 }
--- backend/index.ts
+++ backend/index.ts
@@ -1,12 +1,16 @@
 import { config } from "dotenv";
 import express from 'express';
 import ProductController from "./controllers/ProductController.js";
+import path from 'path'
+import { fileURLToPath } from "url";
 config()
 
 const app = express()
 const port = process.env.PORT
 
 app.get('/api/products', ProductController.getProducts)
+app.get('/api/products/:id', ProductController.getProducts)
+
 
 // Login User
 
@@ -16,9 +20,9 @@ app.get('/api/products', ProductController.getProducts)
 
 // Création Commande
 
-
-app.get(/.*/,(req, res) => {
-    res.send("<h1>Back end is running 👍</h1>")
-})
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+console.log(path.join(__dirname, '..', '..', 'dist'))
+app.get(/.*/, express.static(path.join(__dirname, '..', '..', 'dist')))
 
 app.listen(port, () => console.log(`👂 Serveur listening on http://localhost:${port}`))
